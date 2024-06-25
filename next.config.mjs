bfdd55df42ed2689cc65c9957f15805f40d2a7b4
yasminadelia <yasminadelia11@gmail.com>
2024-06-26 00:38:41 +0700

    chores: adjust next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import withPWA from 'next-pwa';
 
 const nextConfig = {
   reactStrictMode: true,      // Enable React strict mode for improved error handling
-  swcMinify: true,            // Enable SWC minification for improved performance
+  // swcMinify: true,            // Enable SWC minification for improved performance
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
   }
@@ -11,7 +11,8 @@ const nextConfig = {
 
 export default withPWA({
   dest: "public",         // destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
+  disable: process.env.NODE_ENV === "development",
+  // disable: false,       // disable PWA in the development environment
   register: true,         // register the PWA service worker
   skipWaiting: true,      // skip waiting for service worker activation
 })(nextConfig);
