6fa39eb9589023842af15aafae4cedd3554442fe
yasminadelia <yasminadelia11@gmail.com>
2024-06-28 16:41:55 +0700

    feat: setup PWA again

--- next.config.js
+++ next.config.js
@@ -6,11 +6,16 @@ const nextConfig = {
   },
 };
 
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
   // disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 module.exports = withPWA(nextConfig);
