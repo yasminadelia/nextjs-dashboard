6b79d7852c3c127a577da957c3ed3fdefc5d3091
yasminadelia <yasminadelia11@gmail.com>
2024-06-28 13:32:47 +0700

    feat: config PWA

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
 
 const withPWA = withPWAInit({
   dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  // disable: false,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 
