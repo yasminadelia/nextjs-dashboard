c9db5b6ef79239da47bb493941bf2fc76c1f7a3d
yasminadelia <yasminadelia11@gmail.com>
2024-07-04 17:09:13 +0700

    feat: improve accessibility by adding error handler in server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,52 +7,83 @@ import {z} from 'zod'
 
 const FormSchema= z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.'
+  }),
+  amount: z.coerce.number().gt(0, {message: 'Please enter an amount greater than $0.'}),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status. '
+  }),
   date: z.string()
 })
 
 const CreateInvoice = FormSchema.omit({id: true, date: true})
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
- 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-  
-  const {customerId, amount, status} = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validateFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  // console.log({validateFields})
+
+  if(!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice. '
+    }
+  }
+
+  // Prepare data for insertion into the database
+  const {customerId, amount, status} = validateFields.data
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0];
 
+  // Insert data into the database
   try {
     await sql `
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `
   } catch (error) {
+    // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice'
     }
   }
 
-
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
  
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const {customerId, amount, status} = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validateFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if(!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice. '
+    }
+  }
+
+  const {customerId, amount, status} = validateFields.data
   const amountInCents = amount * 100
 
   try {
